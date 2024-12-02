b1bc1e30405ee3edb2bba96deb3e84d7e333331b
Steve <smp_guitar@hotmail.com>
2024-12-01 19:34:20 -0600

    set up for day 2

---
+++ advent_of_code_2024/lib/day_two/day2.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+List<int> sortList(List<int> list) {
+  list.sort((a, b) => a.compareTo(b));
+  return list;
+}
+
+solveDay1() {
+  RegExp regExp = RegExp(r'\S+');
+  String text = File(
+          '/Users/stephenparks/Development/advent_of_code_2024_dart/advent_of_code_2024/lib/day_one/day_1.txt')
+      .readAsStringSync();
+
+  List<String> puzzleInput =
+      regExp.allMatches(text).map((match) => match.group(0)!).toList();
+
+  print(puzzleInput);
+
+  List<int> left = [];
+  List<int> right = [];
+  List<int> answer = [];
+
+  int finalAnswer = 0;
+
+  for (int i = 0; i < puzzleInput.length; i++) {
+    print('i: $i');
+    if (i.isEven) {
+      left.add(int.parse(puzzleInput[i]));
+      print('left: $left');
+    }
+    if (i.isOdd) {
+      right.add(int.parse(puzzleInput[i]));
+      print('right: $right');
+    }
+  }
+
+  left = sortList(left);
+  right = sortList(right);
+
+  if (left.length != right.length) {
+    print('Error: Left list length != Right list length');
+  }
+
+  print('left: $left, right: $right');
+  if (left.length == right.length) {
+    for (int i = 0; i < left.length; i++) {
+      print('i: $i');
+      answer.add((left[i] - right[i]).abs());
+    }
+  }
+
+  for (int item in answer) {
+    finalAnswer = finalAnswer + item;
+  }
+
+  print('answer: $answer');
+  print('final answer: $finalAnswer');
+}
